a65ff2550c5eb66e21ca2342ffa8de77d991b2ef
barillamw98 <mbarill@ncsu.edu>
2024-08-07 17:01:46 -0400

    Adding interactive keyboard

--- src/components/Qwerty.tsx
+++ src/components/Qwerty.tsx
@@ -6,11 +6,20 @@ interface QwertyProps {
 }
 
 const Qwerty: React.FC<QwertyProps> = ({ store }) => {
-    const qwerty = ["qwertyuiop", "asdfghjkl", "zxcvbnm"]
+    const qwerty = ["q w e r t y u i o p", "a s d f g h j k l", "✔ z x c v b n m <-"]
+
+    const handleKeyClick = (key: string) => {
+        if (key === "✔") key ='Enter';
+        else if (key === "<-") key = 'Backspace';
+        
+        const event = new KeyboardEvent('keyup', { key });
+        store.handleKeyup(event);
+    };
+
     return (<div>
         {qwerty.map((row) => (
             <div className ="flex justify-center qwerty">
-                {row.split('').map((char) => {
+                {row.split(' ').map((char) => {
                     const validationLetter = store.exactGuesses.includes(char) 
                       ? 'correctPlacement'
                       : store.inexactGuesses.includes(char)
@@ -20,7 +29,10 @@ const Qwerty: React.FC<QwertyProps> = ({ store }) => {
                       : 'notGuessed'
                 
                     return(
-                        <div className={`m-0.5 flex h-10 w-8 items-center justify-center rounded-md uppercase ${validationLetter} letter`}>
+                        <div 
+                            className={`m-0.5 flex h-10 w-8 items-center justify-center rounded-md uppercase ${validationLetter} letter`}
+                            onClick={() => handleKeyClick(char)}
+                            >
                             {char}
                         </div>
                     )
